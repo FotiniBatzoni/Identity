9a3c1961b30a695e011e7baf03b317915aef76b6
fotini <f.batzoni@gmail.com>
2023-04-19 10:19:30 +0300

    Add project files.

---
+++ Identity/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+
+namespace Identity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var creationResult = userManager.Create(new IdentityUser
+                ("scott@test.com"), "Password123!");
+            Console.WriteLine($"Created: {0}", creationResult.Succeeded);
+            Console.ReadLine();
+        }
+    }
+}
