c22ce9d3e0edb03911485d4bd6afb09341b3837a
fotini <f.batzoni@gmail.com>
2023-04-19 10:58:45 +0300

    Assigning a claim

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
-
+using System.Security.Claims;
 
 namespace Identity
 {
@@ -9,13 +9,20 @@ namespace Identity
     {
         static void Main(string[] args)
         {
+            var username = "scott@test.com";
+            var password = "Password123!";
+
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
 
-            var creationResult = userManager.Create(new IdentityUser
-                ("scott@test.com"), "Password123!");
-            Console.WriteLine($"Created: {0}", creationResult.Succeeded);
-            Console.ReadLine();
+            //var creationResult = userManager.Create(new IdentityUser
+            //    ("scott@test.com"), "Password123!");
+            //Console.WriteLine($"Created: {0}", creationResult.Succeeded);
+            //Console.ReadLine();
+
+            var user = userManager.FindByName(username);
+            var claimResult = userManager.AddClaim(user.Id, new Claim("given_name", "Scott"));
+            Console.WriteLine($"Claim: {0}", claimResult.Succeeded);
         }
     }
 }
